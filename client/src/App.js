1616199b32892cf23d7f1df38b585602e5117091
Kolade-Junaid <koladejun@yahoo.com>
2020-01-28 16:10:33 +0000

    Modified the client/src/App.js and client/src/index.js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+
+        <Route path="/movies/:movieId">
+          <Movie/>
+        </Route>
+      </div>
     </div>
   );
 };
